1fbb297e144886e9ce4cab51ca0803e513862672
ghtak <gwonhyeong.tak@gmail.com>
2019-07-23 17:26:18 +0900

    [title] init

---
+++ tlab/include/tlab/tlab.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file tlab.hpp
+ * @author ghtak (gwonhyeong.tak@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-07-23
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#ifndef __tlab_h__
+#define __tlab_h__
+
+namespace tlab{
+    int interest(void);
+}
+
+#endif
